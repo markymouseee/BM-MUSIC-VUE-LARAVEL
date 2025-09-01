bbdc5df382026a60dccf8130a1a3a25950d21585
Mark <sanamamarknoriel9@gmail.com>
2025-09-02 05:13:33 +0800

    refactor(routes): replace LoginController with AuthenticatedSessionController and add login POST route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
