71bd22d7174c4638bbee2acb9b275fc962e59222
Mark <sanamamarknoriel9@gmail.com>
2025-09-02 02:09:28 +0800

    Enhance registration page with credits, repository links, and terms updates

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -40,6 +40,58 @@ const goToSignIn = () => router.visit('/sign-in');
                 </div>
 
                 <ul class="mt-6 text-sm space-y-2 opacity-95">
+                    <a href="https://github.com/markymouseee/BM-MUSIC-VUE-LARAVEL" target="_blank"
+                        class="hover:underline">
+                        <li class="flex items-center gap-3 mb-2">
+                            <span class="inline-flex items-center justify-center w-7 h-7 bg-white/20 rounded-full">
+
+                                <svg width="20" height="20" viewBox="0 0 20 20" version="1.1"
+                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+
+                                    <title>github [#142]</title>
+                                    <desc>Created with Sketch.</desc>
+                                    <defs>
+                                    </defs>
+                                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                        <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)"
+                                            fill="currentColor">
+                                            <g id="icons" transform="translate(56.000000, 160.000000)">
+                                                <path
+                                                    d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"
+                                                    id="github-[#142]">
+                                                </path>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </svg>
+                            </span>
+                            Github Repository
+                        </li>
+                    </a>
+
+                    <a href="https://github.com/markymouseee" target="_blank" class="hover:underline">
+                        <li class="flex items-center gap-3 mb-2">
+                            <span class="inline-flex items-center justify-center w-7 h-7 bg-white/20 rounded-full">
+                                <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path opacity="0.5"
+                                        d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+                                        fill="currentColor" />
+                                    <path
+                                        d="M13.4881 6.44591C13.8882 6.55311 14.1256 6.96437 14.0184 7.36447L11.4302 17.0237C11.323 17.4238 10.9117 17.6613 10.5116 17.5541C10.1115 17.4468 9.8741 17.0356 9.98131 16.6355L12.5695 6.97624C12.6767 6.57614 13.088 6.3387 13.4881 6.44591Z"
+                                        fill="currentColor" />
+                                    <path
+                                        d="M14.9697 8.46967C15.2626 8.17678 15.7374 8.17678 16.0303 8.46967L16.2387 8.67801C16.874 9.3133 17.4038 9.84308 17.7678 10.3202C18.1521 10.8238 18.4216 11.3559 18.4216 12C18.4216 12.6441 18.1521 13.1762 17.7678 13.6798C17.4038 14.1569 16.874 14.6867 16.2387 15.322L16.0303 15.5303C15.7374 15.8232 15.2626 15.8232 14.9697 15.5303C14.6768 15.2374 14.6768 14.7626 14.9697 14.4697L15.1412 14.2981C15.8229 13.6164 16.2797 13.1574 16.5753 12.7699C16.8577 12.3998 16.9216 12.1843 16.9216 12C16.9216 11.8157 16.8577 11.6002 16.5753 11.2301C16.2797 10.8426 15.8229 10.3836 15.1412 9.70191L14.9697 9.53033C14.6768 9.23744 14.6768 8.76257 14.9697 8.46967Z"
+                                        fill="currentColor" />
+                                    <path
+                                        d="M7.96986 8.46967C8.26275 8.17678 8.73762 8.17678 9.03052 8.46967C9.32341 8.76257 9.32341 9.23744 9.03052 9.53033L8.85894 9.70191C8.17729 10.3836 7.72052 10.8426 7.42488 11.2301C7.14245 11.6002 7.07861 11.8157 7.07861 12C7.07861 12.1843 7.14245 12.3998 7.42488 12.7699C7.72052 13.1574 8.17729 13.6164 8.85894 14.2981L9.03052 14.4697C9.32341 14.7626 9.32341 15.2374 9.03052 15.5303C8.73762 15.8232 8.26275 15.8232 7.96986 15.5303L7.76151 15.322C7.12617 14.6867 6.59638 14.1569 6.23235 13.6798C5.84811 13.1762 5.57861 12.6441 5.57861 12C5.57861 11.3559 5.84811 10.8238 6.23235 10.3202C6.59638 9.84308 7.12617 9.31331 7.76151 8.67801L7.96986 8.46967Z"
+                                        fill="currentColor" />
+                                </svg>
+                            </span>
+                            Developed by: Mark
+                        </li>
+                    </a>
+
                     <li class="flex items-center gap-3">
                         <span class="inline-flex items-center justify-center w-7 h-7 bg-white/20 rounded-full">
                             <svg width="20" height="20" viewBox="0 0 24 24">
@@ -100,10 +152,11 @@ const goToSignIn = () => router.visit('/sign-in');
 
                     <p class="text-xs text-gray-500 dark:text-gray-400 leading-relaxed">
                         By continuing, you agree to our
-                        <a href="#" class="text-blue-600 dark:text-blue-400 hover:underline">terms and conditions</a>.
+                        <a href="#" class="text-blue-600 dark:text-blue-400 hover:underline">Terms of Service</a> and <a
+                            href="#" class="text-blue-600 dark:text-blue-400 hover:underline">Privacy Policy</a>.
                     </p>
 
-                    <PrimaryButton type="submit" class="w-full py-3 text-base font-semibold transition">
+                    <PrimaryButton type="submit">
                         Sign up
                     </PrimaryButton>
 
