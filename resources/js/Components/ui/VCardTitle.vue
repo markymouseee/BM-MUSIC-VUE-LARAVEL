d6b48f3b5c474b6bde50e9837e1936235ed52253
Mark <sanamamarknoriel9@gmail.com>
2025-09-03 05:16:54 +0800

    Adds VCardTitle component
    Introduces a new VCardTitle component for use in cards.
    
    This component provides a consistent styling and structure for card titles, leveraging a slot for content and allowing for custom class names.

---
+++ resources/js/Components/ui/VCardTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <h4
+    data-slot="card-title"
+    :class="cn('leading-none', className)"
+  >
+    <slot />
+  </h4>
+</template>
+
+<script setup lang="ts">
+import { cn } from './utils';
+
+interface Props {
+  className?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  className: ''
+});
+</script>
