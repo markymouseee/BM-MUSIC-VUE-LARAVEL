c25d6f3ad3fa177a19ee18c5ce939fbd98730880
Mark <sanamamarknoriel9@gmail.com>
2025-09-03 05:16:18 +0800

    Adds utility functions for class name management
    Introduces a utility file with functions for managing class names using `clsx` and `tailwind-merge`.
    
    This provides a convenient way to handle conditional class names in Vue components. Also exports Vue.js specific utility types.

---
+++ resources/js/Components/ui/utils.ts
@@ -0,0 +1,9 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// Vue.js specific utility types
+export type VueClassValue = ClassValue;
