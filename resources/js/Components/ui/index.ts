0b66c92100f2a4d7dd870fb3568240af53b15202
Mark <sanamamarknoriel9@gmail.com>
2025-09-03 05:15:58 +0800

    Exports Vue UI components
    Exports all Vue UI components and a utility function to make them available for use throughout the application.

---
+++ resources/js/Components/ui/index.ts
@@ -0,0 +1,14 @@
+// Export all Vue UI components
+export { default as VCard } from './VCard.vue';
+export { default as VCardHeader } from './VCardHeader.vue';
+export { default as VCardTitle } from './VCardTitle.vue';
+export { default as VCardContent } from './VCardContent.vue';
+export { default as VButton } from './VButton.vue';
+export { default as Card } from './card.vue';
+export { default as Button } from './button.vue';
+export { default as Avatar } from './avatar.vue';
+export { default as Input } from './input.vue';
+export { default as Badge } from './badge.vue';
+
+// Export utilities
+export { cn } from './utils';
