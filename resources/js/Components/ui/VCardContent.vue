15b0ed4f5ec910e0383eaf0d18a346e3327709fb
Mark <sanamamarknoriel9@gmail.com>
2025-09-03 05:16:42 +0800

    Adds VCardContent component
    Adds a reusable VCardContent component.
    
    This component provides a consistent styling and structure
    for card content sections, including padding and ensuring
    consistent bottom padding for the last child element.

---
+++ resources/js/Components/ui/VCardContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    data-slot="card-content"
+    :class="cn('px-6 [&:last-child]:pb-6', className)"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { cn } from './utils';
+
+interface Props {
+  className?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  className: ''
+});
+</script>
