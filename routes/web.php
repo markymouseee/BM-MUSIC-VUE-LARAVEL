9d20f238df94853a96bf01e8678cd4fb795874b1
Mark <sanamamarknoriel9@gmail.com>
2025-09-05 02:43:54 +0800

    Feat: Add user registration route and update web routes

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,33 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Index');
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('User/DashboardUser');
-});
+Route::get('/', fn() => Inertia::render('Index'))
+    ->middleware('redirect.auth.role');
 
 Route::middleware('guest')->group(function () {
     Route::get('/sign-in', [AuthenticatedSessionController::class, 'create'])
         ->name('user.login');
 
-    Route::get('/sign-up', [UserController::class, 'create'])
+    Route::get('/sign-up', [RegisteredUserController::class, 'create'])
         ->name('user.register');
-
 
     Route::post('/sign-in', [AuthenticatedSessionController::class, 'store'])
         ->name('user.login.store');
+
+    Route::post('/sign-up', [RegisteredUserController::class, 'store'])
+        ->name('user.register.store');
+});
+
+Route::prefix('user')->middleware('auth')->group(function () {
+    Route::get('/dashboard', fn() => Inertia::render('User/DashboardUser'))
+        ->name('user.dashboard');
+});
+
+Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
+    Route::get('/dashboard', fn() => Inertia::render('User/DashboardUser'))
+        ->name('admin.dashboard');
 });
