e51fdb08d10705b467896682765c2dc157c622dd
Mark <sanamamarknoriel9@gmail.com>
2025-09-05 02:43:27 +0800

    Adds registration request validation
    Adds a new request class for handling user registration.
    
    This request class defines the validation rules for the registration form,
    including requirements for fullname, email, password, and password confirmation.
    It also defines custom error messages for each validation rule to improve user experience.

---
+++ app/Http/Requests/Auth/RegistrationRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegistrationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'fullname' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'password_confirmation' => 'required|same:password',
+        ];
+    }
+
+
+    public function messages(): array
+    {
+        return [
+            'fullname.required' => 'Fullname is required',
+            'email.required' => 'Email is required',
+            'email.email' => 'Email is not valid',
+            'email.unique' => 'Email is already taken',
+            'password.required' => 'Password is required',
+            'password.min' => 'Password must be at least 6 characters',
+            'password_confirmation.required' => 'Password confirmation is required',
+            'password_confirmation.same' => 'Password confirmation does not match',
+        ];
+    }
+}
