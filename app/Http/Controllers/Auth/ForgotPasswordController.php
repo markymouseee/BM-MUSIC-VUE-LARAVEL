5b31b0bc36b71a7aee978446f995a08976993113
Mark <sanamamarknoriel9@gmail.com>
2025-09-08 02:48:35 +0800

    Adds forgot password functionality
    Adds a controller for handling forgot password requests.
    
    This controller will be responsible for receiving the user's email address and sending a password reset link.

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ForgotPasswordController extends Controller
+{
+    public function store(Request $request)
+    {
+        // Send password reset link to user's email
+    }
+}
