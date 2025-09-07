2f9b13a9945457a172e303f04ada2a1663d07bf8
Mark <sanamamarknoriel9@gmail.com>
2025-09-08 02:14:14 +0800

    feat: Implement password visibility toggle in InputField component

--- resources/js/Components/inputs/InputField.vue
+++ resources/js/Components/inputs/InputField.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 
 const props = defineProps({
     id: {
@@ -49,19 +51,52 @@ const updateValue = (event: Event) => {
         emit("clearError");
     }
 };
+
+const showPassword = ref(false);
+
+const toggleShow = () => {
+    showPassword.value = !showPassword.value;
+}
 </script>
 
 <template>
     <div>
-        <label :for="props.id" :class="['block text-sm font-medium mb-2', props.error ?
-            'text-red-700 dark:text-red-500' : 'text-gray-700 dark:text-gray-300']">
-            {{ props.label }}
-        </label>
-        <input :type="props.type" :name="props.name" :id="props.id" :placeholder="props.placeholder"
-            :value="props.modelValue" @input="updateValue" :class="['w-full px-4 py-2 border rounded-lg dark:bg-gray-700 pr-12 focus:outline-none', props.error ?
-                ' text-red-700 dark:text-red-500 focus:ring-red-500 dark:border-red-500' :
-                ' dark:border-gray-600 dark:text-gray-100 focus:from-blue-600 focus:to-purple-600']" />
+        <div class="relative">
+            <label :for="props.id" :class="['block text-sm font-medium mb-2', props.error ?
+                'text-red-700 dark:text-red-500' : 'text-gray-700 dark:text-gray-300']">
+                {{ props.label }}
+            </label>
+            <input :type="props.type === 'password' ? showPassword ? 'text' : 'password' : props.type"
+                :name="props.name" :id="props.id" :placeholder="props.placeholder" :value="props.modelValue"
+                @input="updateValue" :class="['w-full px-4 py-2 border rounded-lg dark:bg-gray-700 pr-12 focus:outline-none', props.error ?
+                    ' text-red-700 dark:text-red-500 focus:ring-red-500 dark:border-red-500' :
+                    ' dark:border-gray-600 dark:text-gray-100 focus:from-blue-600 focus:to-purple-600']" />
 
+            <button type="button" v-if="type === 'password'" @click="toggleShow"
+                class="absolute inset-y-0 right-0 top-6 pr-3 flex items-center text-sm text-gray-500 dark:text-gray-500">
+                <svg v-if="!showPassword" fill="currentColor" height="22" width="22" version="1.1" id="Capa_1"
+                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 612 612"
+                    xml:space="preserve">
+                    <g>
+                        <g>
+                            <path d="M609.608,315.426c3.19-5.874,3.19-12.979,0-18.853c-58.464-107.643-172.5-180.72-303.607-180.72
+			S60.857,188.931,2.393,296.573c-3.19,5.874-3.19,12.979,0,18.853C60.858,423.069,174.892,496.147,306,496.147
+			S551.143,423.069,609.608,315.426z M306,451.855c-80.554,0-145.855-65.302-145.855-145.855S225.446,160.144,306,160.144
+			S451.856,225.446,451.856,306S386.554,451.855,306,451.855z" />
+                            <path d="M306,231.67c-6.136,0-12.095,0.749-17.798,2.15c5.841,6.76,9.383,15.563,9.383,25.198c0,21.3-17.267,38.568-38.568,38.568
+			c-9.635,0-18.438-3.541-25.198-9.383c-1.401,5.703-2.15,11.662-2.15,17.798c0,41.052,33.279,74.33,74.33,74.33
+			s74.33-33.279,74.33-74.33S347.052,231.67,306,231.67z" />
+                        </g>
+                    </g>
+                </svg>
+
+                <svg v-else fill="currentColor" width="22" height="22" viewBox="0 -64 640 640"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z" />
+                </svg>
+            </button>
+        </div>
         <div v-if="errorMessage != ''" class="text-red-500 text-sm mt-2">
             <span>
                 {{ props.errorMessage }}
